31ed6255ba2ba21353b5f7d2fcec4a57c7ed35bf
Saint Dev <100862990+std447@users.noreply.github.com>
2024-12-22 21:37:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const { argv } = require("node:process")
 const {SUPER_SECRET: secret = "no secret"} = process.env;
 console.log("secret ",secret);
 
+const output = (input) => `This is your ${input}`;
+
+console.log(output(secret));
+
 // console.log("argv",Array.from(argv));
 const args = Array.from(argv);
 const date = args[args.indexOf("--date") + 1]
